2d347e278a2f429f71099073a088b7d1a9cd8689
sh <junsh@126.com>
2022-07-16 21:01:03 +0800

    换一种更容易理解的方法来实现同样的功能

--- src/main/java/com/sdk4/args/SingleValuedOptionParser.java
+++ src/main/java/com/sdk4/args/SingleValuedOptionParser.java
@@ -2,6 +2,7 @@ package com.sdk4.args;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingleValuedOptionParser<T> implements OptionParser<T> {
     Function<String, T> valueParser;
@@ -19,27 +20,25 @@ class SingleValuedOptionParser<T> implements OptionParser<T> {
             return defaultValue;
         }
 
-        if (isReachEndOfList(arguments, index)
-                || isFollowedByOtherFlag(arguments, index)) {
+        List<String> values = values(arguments, index);
+
+        if (values.size() < 1) {
             throw new InsufficientArgumentsException(option.value());
         }
-        if (secondArgumentIsNotAFlag(arguments, index)) {
+        if (values.size() > 1) {
             throw new TooManyArgumentsException(option.value());
         }
+
         String value = arguments.get(index + 1);
         return valueParser.apply(value);
     }
 
-    private boolean secondArgumentIsNotAFlag(List<String> arguments, int index) {
-        return index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-");
-    }
+    private List<String> values(List<String> arguments, int index) {
+        int followingFlag = IntStream.range(index + 1, arguments.size())
+                .filter(it -> arguments.get(it).startsWith("-"))
+                .findFirst().orElse(arguments.size());
 
-    private boolean isFollowedByOtherFlag(List<String> arguments, int index) {
-        return arguments.get(index + 1).startsWith("-");
-    }
-
-    private boolean isReachEndOfList(List<String> arguments, int index) {
-        return index + 1 == arguments.size();
+        return arguments.subList(index + 1, followingFlag);
     }
 
 }
