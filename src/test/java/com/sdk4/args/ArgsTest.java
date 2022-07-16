10d01cf3d87b4395fb62963259be73cd07cc8153
sh <junsh@126.com>
2022-07-16 12:19:09 +0800

    组件构思

--- src/test/java/com/sdk4/args/ArgsTest.java
+++ src/test/java/com/sdk4/args/ArgsTest.java
@@ -3,7 +3,9 @@ package com.sdk4.args;
 import org.junit.jupiter.api.Test;
 
 public class ArgsTest {
-
+    // -l -p 8080 -d /usr/logs
+    // [-l], [-p, 8080], [-d, /usr/logs]
+    // {-l:[], -p:[8080], -d:[/usr/logs]}
     @Test
     void should() {
         Arguments args = Args.parse("l:b,p:d,d:s", "-l", "-p", "8080", "-d", "/usr/logs");
@@ -13,6 +15,18 @@ public class ArgsTest {
         Options options = Args.parse(Option.class, "-l", "-p", "8080", "-d", "/usr/logs");
         options.logging();
         options.port();
+    }
+
+    static class Options() {
+        boolean logging;
+        int port;
+        String directory;
+
+        public Options(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+            this.logging = logging;
+            this.port = port;
+            this.directory = directory;
+        }
     }
 
 }
