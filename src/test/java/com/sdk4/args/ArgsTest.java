ef8be5bbcabe796a08e2c7c6255c9546a267dbd4
sh <junsh@126.com>
2022-07-16 11:52:50 +0800

    组件构思

---
+++ src/test/java/com/sdk4/args/ArgsTest.java
@@ -0,0 +1,17 @@
+package com.sdk4.args;
+
+import org.junit.jupiter.api.Test;
+
+public class ArgsTest {
+
+    @Test
+    void should() {
+        Arguments args = Args.parse("l:b,p:d,d:s", "-l", "-p", "8080", "-d", "/usr/logs");
+        args.getBool("l");
+        args.getInt("p");
+
+        Options options = Args.parse(Option.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        options.logging();
+        options.port();
+    }
+}
